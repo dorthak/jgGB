8cd4ea2c09cd0a7ccc9422372a3d84ab33bb84c5
Juliean Galak <jgalak@jgalak.com>
2025-06-03 22:33:16 -0400

    Implementing basic PPU functionality, to support SST tests

--- jgGB/ppu.cpp
+++ jgGB/ppu.cpp
@@ -1,1 +1,44 @@
 #include "ppu.h"
+
+ppu::ppu()
+{
+	//this->video_buffer = (uint32_t *) malloc(YRES * XRES * sizeof(uint32_t));
+	this->video_buffer = new(uint32_t[YRES * XRES]);
+}
+
+ppu::~ppu()
+{
+	delete(video_buffer);
+}
+
+void ppu::ppu_init()
+{
+
+}
+
+void ppu::ppu_vram_write(uint16_t address, uint8_t value)
+{
+	vram[address - 0x8000] = value;
+}
+uint8_t ppu::ppu_vram_read(uint16_t address)
+{
+	return vram[address - 0x8000];
+}
+
+void ppu::ppu_oam_write(uint16_t address, uint8_t value)
+{
+	if (address >= 0xFE00)
+	{
+		address -= 0xFE00;
+	}
+	oam_ram[address] = value;
+}
+uint8_t ppu::ppu_oam_read(uint16_t address)
+{
+	if (address >= 0xFE00)
+	{
+		address -= 0xFE00;
+	}
+
+	return oam_ram[address];
+}
--- jgGB/ppu.h
+++ jgGB/ppu.h
@@ -1,10 +1,29 @@
 #pragma once
 #include "common.h"
 
+static const int LINES_PER_FRAME = 154;
+static const int TICKS_PER_LINE = 456;
+static const int YRES = 144;
+static const int XRES = 160;
+
 class ppu
 {
 public:
+	ppu();
+	~ppu();
 	void ppu_init();
 	void ppu_tick();
+
+	void ppu_vram_write(uint16_t address, uint8_t value);
+	uint8_t ppu_vram_read(uint16_t address);
+	void ppu_oam_write(uint16_t address, uint8_t value);
+	uint8_t ppu_oam_read(uint16_t address);
+
+private:
+	uint32_t* video_buffer;
+	uint8_t vram[0x2000];
+
+	//TODO: This is temporary
+	uint8_t oam_ram[0xA0];
 };
 
